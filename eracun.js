67c8746da9854417cb00fc7fc2843b0eb79a109b
lukagustin <lg2609@student.uni-lj.si>
2016-05-08 12:01:53 +0000

    Začetek dela na III. nalogi

--- eracun.js
+++ eracun.js
@@ -133,23 +133,42 @@ var pesmiIzRacuna = function(racunId, callback) {
     Track.TrackId IN (SELECT InvoiceLine.TrackId FROM InvoiceLine, Invoice \
     WHERE InvoiceLine.InvoiceId = Invoice.InvoiceId AND Invoice.InvoiceId = " + racunId + ")",
     function(napaka, vrstice) {
-      console.log(vrstice);
+      callback(napaka,vrstice);
     })
 }
 
 // Vrni podrobnosti o stranki iz računa
 var strankaIzRacuna = function(racunId, callback) {
     pb.all("SELECT Customer.* FROM Customer, Invoice \
-            WHERE Customer.CustomerId = Invoice.CustomerId AND Invoice.InvoiceId = " + racunId,
+            WHERE Customer.CustomerId = Invoice.CustomerId AND Invoice.InvoiceId = " + racunId, 
     function(napaka, vrstice) {
-      console.log(vrstice);
+      callback(napaka, vrstice);
     })
 }
 
 // Izpis računa v HTML predstavitvi na podlagi podatkov iz baze
 streznik.post('/izpisiRacunBaza', function(zahteva, odgovor) {
-  odgovor.end();
-})
+  var form = new formidable.IncomingForm();
+  form.parse(zahteva, function (napaka1, polja, datoteke) {
+    if(napaka1) odgovor.sendStatus(500);
+    var racun = polja.seznamRacunov;
+    strankaIzRacuna(racun, function(napaka2, strankaRacuna1) {
+      if(napaka2) odgovor.sendStatus(500);
+      pesmiIzRacuna(racun, function(napaka3, pesmiRacuna) {
+        if(napaka3) odgovor.sendStatus(500);
+        //console.log(strankaRacuna1[0]);
+        //console.log(pesmiRacuna);
+        odgovor.setHeader('content-type', 'text/xml');
+        odgovor.render('eslog', {
+        vizualiziraj: 'html', 
+        postavkeRacuna: pesmiRacuna,
+        strankaRacuna: strankaRacuna1[0]
+        });
+    });
+  });
+  //odgovor.end();
+});
+});
 
 // Izpis računa v HTML predstavitvi ali izvorni XML obliki
 streznik.get('/izpisiRacun/:oblika', function(zahteva, odgovor) {
